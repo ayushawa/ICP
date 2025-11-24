f312ac2f68aa54e56380b5cf89b6e315efcccf36
ayushawasthi <devkali849@gmail.com>
2025-11-24 20:41:24 +0530

    Time: 6 ms (46.10%) | Memory: 75.9 MB (38.45%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,19 +1,19 @@
 class Solution {
-   
     public int maxProfit(int[] arr) {
-        int n  = arr.length;
-        int prev[][] = new int[3][2];
-        int curr[][] = new int[3][2];
+        int n = arr.length;
+        int prev[] = new int[5];
+        int curr[] = new int[5];
         for(int i=n-1;i>=0;i--){
-            for(int cap = 1;cap<3;cap++){
-                for(int buy = 0;buy<2;buy++){
-                    if(buy == 1) curr[cap][buy] = Math.max(-arr[i]+prev[cap][0],curr[cap][buy]);
-                     else  curr[cap][buy] = Math.max(arr[i]+prev[cap-1][1],curr[cap][buy]);
+            for(int trans = 3;trans>=0;trans--){
+                if(trans%2 == 0){
+                    // buy 
+                    curr[trans] = Math.max(-arr[i] + prev[trans+1],prev[trans]);
                 }
-               
+                else curr[trans] = Math.max(arr[i]+ prev[trans + 1],prev[trans]);
             }
             prev = curr;
         }
-        return prev[2][1];
+
+        return prev[0];
     }
 }
