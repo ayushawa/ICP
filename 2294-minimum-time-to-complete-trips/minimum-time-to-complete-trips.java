37630565ce91bd4a6f1dbe278f5fa7b74a49a073
ayushawasthi <devkali849@gmail.com>
2025-12-26 12:39:25 +0530

    Time: 47 ms (90.65%) | Memory: 90.9 MB (68.71%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -1,30 +1,28 @@
 class Solution {
-    public boolean isPossibe(int arr[],long m,int t){
-        long count =0;
-        for(int x: arr){
-            count+= m/x;
-            if(count>=t) return true;
+    public boolean completeTrip(int time[],long t,int totalTrips){
+        long count = 0;
+        for(int x: time){
+            count+= t/(long)x;
+            if(count>= totalTrips) return true;
         }
-        return count>=t;
-
+        return false;
     }
     public long minimumTime(int[] time, int totalTrips) {
-        long ans = 0;
-        long s = time[0];
-        long e = (long)1e14;
-        for(int x: time){
-            s = Math.min(s,x);
-        }
-        while(s<=e){
-            long m = s + (e-s)/2;
-            if(isPossibe(time,m,totalTrips)){
-                ans = m;
-                e = m-1;
-            }
-            else {
-                s = m+1;
-            }
-        }
-        return ans;
+       long s = 1;
+       long e =0;
+       for(int x: time){
+           e = Math.max(e,x);
+       } 
+       e = e *totalTrips;
+       long ans = 100000007;
+       while(s<=e){
+         long m = s + (e-s)/2;
+         if(completeTrip(time,m,totalTrips)){
+            ans = m;
+            e = m-1;
+         }
+         else s = m+1;
+       }
+       return ans;
     }
 }
