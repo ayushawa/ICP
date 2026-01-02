5d246cc2f1cc3b200ebaf9144a6d8ec3ac29534f
ayushawasthi <devkali849@gmail.com>
2026-01-02 15:31:15 +0530

    Time: 75 ms (75.00%) | Memory: 201.3 MB (78.57%) - LeetSync

--- 3389-minimum-time-to-visit-disappearing-nodes/minimum-time-to-visit-disappearing-nodes.java
+++ 3389-minimum-time-to-visit-disappearing-nodes/minimum-time-to-visit-disappearing-nodes.java
@@ -8,8 +8,8 @@ class Touple{
 }
 class Pair{
     int node;
-    long t;
-    Pair(int node,long t){
+    int t;
+    Pair(int node,int t){
         this.node = node;
         this.t = t;
     }
@@ -24,23 +24,23 @@ class Solution {
             adj.get(edges[i][0]).add(new Touple(edges[i][1],edges[i][2],disappear[edges[i][1]]));
             adj.get(edges[i][1]).add(new Touple(edges[i][0],edges[i][2],disappear[edges[i][0]]));
         }
-        long times[] = new long[n];
-        Arrays.fill(times,Long.MAX_VALUE);
+        int times[] = new int[n];
+        Arrays.fill(times,Integer.MAX_VALUE);
         
-        PriorityQueue<Pair> q =new PriorityQueue<>((a,b)->Long.compare(a.t,b.t));
-        q.add(new Pair(0,(long)0));
+        PriorityQueue<Pair> q =new PriorityQueue<>((a,b)->a.t-b.t);
+        q.add(new Pair(0,0));
         times[0] = 0;
         while(!q.isEmpty()){
             Pair  p = q.poll();
             int node = p.node;
-            long t= p.t;
+            int t= p.t;
             if (t > times[node]) continue;
             for(Touple top: adj.get(node)){
                 int node2 = top.node;
                 int time = top.time;
                 int lostTime = top.lost;
 
-                long totaltime = time + t;
+                int totaltime = time + t;
                 if(times[node2]>totaltime && totaltime<lostTime){
                     times[node2] = totaltime;
                     q.add(new Pair(node2,totaltime));
@@ -48,12 +48,11 @@ class Solution {
             }
         }
 
-        int ans[] = new int[n];
-
+   
         for(int i=0;i<n;i++){
-            if(times[i] == Long.MAX_VALUE) ans[i] = -1;
-            else ans[i] = (int)times[i];
+            if(times[i] == Integer.MAX_VALUE) times[i] = -1;
+           
         }
-        return ans;
+        return times;
     }
 }
