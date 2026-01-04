411c62b6bbc63fbcbb551fe6059775d4e0305edb
ayushawasthi <devkali849@gmail.com>
2026-01-04 18:15:20 +0530

    Time: 1 ms (85.61%) | Memory: 43.3 MB (37.30%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int helper(String s,int i,int dp[]){
-        
-        if( i == s.length()){
-            return 1;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int skip_one  = 0;
-        if(s.charAt(i)!='0'  )skip_one = helper(s,i+1,dp);
-        int skip_two  = 0;
-        if(i!=s.length()-1 && s.charAt(i)!='0' &&  s.substring(i,i+2).compareTo("26")<=0){
-            skip_two = helper(s,i+2,dp);
-        }
-        return dp[i] =  skip_one + skip_two;
-    }
     public int numDecodings(String s) {
         int n = s.length();
         if(s.charAt(0) == '0') return 0;
-        int dp[] = new int[n];
-        Arrays.fill(dp,-1);
-        return  helper(s,0,dp);
+        int dp[] = new int[n+1];
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
+            int skip_one = 0;
+            if(s.charAt(i)!='0') skip_one = dp[i+1];
+            int skip_two = 0;
+            if(i!=s.length()-1 && s.charAt(i)!='0' &&  s.substring(i,i+2).compareTo("26")<=0){
+                 skip_two = dp[i+2];
+             }
+             dp[i] = skip_one + skip_two;
+
+        }
+        
+        return  dp[0];
     }
 }
