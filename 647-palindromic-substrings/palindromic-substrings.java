40d354fe4ff3b0ca184f9fa50ed577e661488f3c
ayushawasthi <devkali849@gmail.com>
2026-01-04 12:35:56 +0530

    Time: 829 ms (8.51%) | Memory: 47 MB (13.52%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrom(String s){
+
+        int i = 0;
+        int j= s.length()-1;
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n= s.length();
+        for(int i =0;i<n;i++){
+            count++;
+            for(int j=i+1;j<n;j++){
+                if(isPalindrom(s.substring(i,j+1))) count++;
+            }
+        }
+        return count;
+    }
+}
