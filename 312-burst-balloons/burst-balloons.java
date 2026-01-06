ffec3eb6d47835d685c75bcdfc38d1a8d8fc0488
ayushawasthi <devkali849@gmail.com>
2026-01-06 20:39:59 +0530

    Time: 40 ms (93.87%) | Memory: 44.4 MB (65.47%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int helper(int arr[],int i,int j,int t[][]){
-        if(i>j) return 0;
-        if(t[i][j] != -1) return t[i][j];
-        int maxi = 0;
-        for(int k=i;k<=j;k++){
-            maxi = Math.max(maxi,arr[i-1]*arr[k]*arr[j+1]+helper(arr,i,k-1,t)+helper(arr,k+1,j,t));
-        }
-        return t[i][j] = maxi;
-    }
     public int maxCoins(int[] nums) {
-      int n= nums.length;
-      int arr[] = new int[n+2];
-      arr[0] = 1;
-      arr[n+1] = 1;
-      for(int i=0;i<n;i++){
-        arr[i+1] = nums[i];
-      }   
-      int t[][] = new int[n+2][n+2];
-      for(int i=0;i<n+2;i++){
-        Arrays.fill(t[i],-1);
-      }
+        int n = nums.length;
+        int arr[] = new int[n+2];
+        arr[0] = 1;
+        arr[n+1] = 1;
+        for(int i=0;i<n;i++){
+            arr[i+1] = nums[i];
+        }
+        int dp[][] = new int[n+2][n+2];
+        for(int i =n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                int maxi = 0;
+                for(int k=i;k<=j;k++){
+                    int cost = arr[i-1]*arr[k]*arr[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi = Math.max(maxi,cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
 
-      return helper(arr,1,n,t);
+        return dp[1][n];
     }
 }
