77f7a51087459be640b8b2020b1e81f69a98ad1a
ayushawasthi <devkali849@gmail.com>
2026-01-07 16:52:26 +0530

    Time: 2 ms (10.95%) | Memory: 47 MB (9.28%) - LeetSync

--- 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/longest-unequal-adjacent-groups-subsequence-i.java
@@ -1,40 +1,34 @@
 class Solution {
-    public int helper(String words[],int i,int prev,int groups[],List<String> l,List<List<String>> ans,int dp[][]){
-        if(i == words.length){
-            if(l.size() != 0){
-                ans.add(new ArrayList<>(l));
-            }
-            return 0;
-        }
-        if( prev!=-1 && dp[i][prev] !=-1) return dp[i][prev];
-
-        int take  = 0;
-        if(prev == -1 || groups[i] !=groups[prev]){
-            l.add(words[i]);
-           take = 1 + helper(words,i+1,i,groups,l,ans,dp);
-            l.remove(l.size()-1);
-        }
-       int nottake =  helper(words,i+1,prev,groups,l,ans,dp);
-       if(prev!=-1)  dp[i][prev] =  Math.max(take,nottake);
-       return   Math.max(take,nottake);
-    }
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
-        List<List<String>> ans = new ArrayList<>();
         int n = words.length;
-        int dp[][] = new int[n][n];
+        int dp[] = new int[n];
+        int path[] = new int[n];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i] = 1;
+            path[i] = i;
         }
-        helper(words,0,-1,groups,new ArrayList<>(),ans,dp);
-        int idx = 0;
-        for(int i=0;i<ans.size();i++){
-            if(ans.get(i).size()>ans.get(i).size()){
-                idx = i;
+        int ind = 0;
+        int maxi = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(groups[i] != groups[j]){
+                    if(dp[i]<dp[j]+1){
+                        dp[i] = dp[j] + 1;
+                        path[i] = j;
+                    }
+                }
+            }
+            if(dp[i]>maxi){
+                maxi = dp[i];
+                ind = i;
             }
         }
-        helper(words,0,-1,groups,new ArrayList<>(),ans,dp);
-        return ans.get(idx);
-
-        
+        List<String> l =new ArrayList<>();
+        l.add(words[ind]);
+        while(ind!=path[ind]){
+            l.add(0,words[path[ind]]);
+            ind = path[ind];
+        }
+        return l;
     }
 }
