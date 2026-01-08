da4269616cc351a621110dfff1fcf231cd0ee247
ayushawasthi <devkali849@gmail.com>
2026-01-09 00:12:21 +0530

    Time: 10 ms (81.63%) | Memory: 47.3 MB (33.94%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,35 +1,30 @@
 class Solution {
-    public boolean isAnagram(int ar1[],int ar2[]){
+    public boolean isAnagram(int map1[],int map2[]){
         for(int i=0;i<26;i++){
-            if(ar1[i] != ar2[i]) return false;
+            if(map1[i]!=map2[i]) return false;
         }
         return true;
     }
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        if(p.length() > s.length()) return list;
-        int map1[] = new int[26];
-        int map2[] = new int[26];
-        for(int i=0;i<p.length();i++){
-            map1[p.charAt(i)-'a']++;
-        }
         int k = p.length();
-        for(int i=0;i<k;i++){
-            map2[s.charAt(i)-'a']++;
-        }
-        if(isAnagram(map1,map2)){
-            list.add(0);
-        }
-        for(int i=k;i<s.length();i++){
-            char ch = s.charAt(i);
-            char temp = s.charAt(i-k);
+        List<Integer> list =new ArrayList<>();
+        if(p.length()>s.length()) return list;
+        int map1[] = new int[26];
+        int map2[] =new int[26];
+        for(char ch : p.toCharArray()){
             map2[ch-'a']++;
-            map2[temp-'a']--;
-            if(isAnagram(map1,map2)){
-                list.add(i-k+1);
-            }
+        }
+
+        for(int i=0;i<k;i++){
+            map1[s.charAt(i)-'a']++;
+        }
+        if(isAnagram(map1,map2)) list.add(0);
+
+        for(int i=k;i<s.length();i++){
+            map1[s.charAt(i)-'a']++;
+            map1[s.charAt(i-k)-'a']--;
+            if(isAnagram(map1,map2)) list.add(i-k+1);
         }
         return list;
-
     }
 }
