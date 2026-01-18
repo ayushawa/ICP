288ce6d222a57f3d37aee173e5cce655e4a3f2f4
ayushawasthi <devkali849@gmail.com>
2026-01-18 19:45:12 +0530

    Time: 1401 ms (13.15%) | Memory: 282.7 MB (17.14%) - LeetSync

---
+++ 823-split-array-with-same-average/split-array-with-same-average.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean splitArraySameAverage(int[] nums) {
+        int n= nums.length;
+        int sum =0;
+        for(int x : nums){
+            sum+=x;
+        }
+
+        List<Set<Integer>> dp = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            dp.add(new HashSet<>());
+        }
+        dp.get(0).add(0);
+
+        for(int x : nums){
+           for(int k = n-1;k>0;k--){
+              for(int prevsum: dp.get(k-1)){
+                  dp.get(k).add(prevsum + x);
+              }
+           }
+        }
+
+        for(int k=1;k<n;k++){
+            if((sum*k)%n == 0){
+                int target = (sum*k)/n;
+                 if(dp.get(k).contains(target)) return true;
+            }
+        }
+
+        return false;
+    }
+}
