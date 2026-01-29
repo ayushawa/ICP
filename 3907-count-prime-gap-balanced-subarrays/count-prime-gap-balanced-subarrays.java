926533ec580f111d092eda2c8578f3b44532d3de
ayushawasthi <devkali849@gmail.com>
2026-01-30 01:08:19 +0530

    Time: 606 ms (47.00%) | Memory: 88.8 MB (24.78%) - LeetSync

--- 3907-count-prime-gap-balanced-subarrays/count-prime-gap-balanced-subarrays.java
+++ 3907-count-prime-gap-balanced-subarrays/count-prime-gap-balanced-subarrays.java
@@ -1,66 +1,77 @@
 class Solution {
-    public int primeSubarray(int[] nums, int k) {
-        int n = nums.length;
-
-        int maxVal = 0;
-        for (int x : nums) maxVal = Math.max(maxVal, x);
-
-        boolean[] isPrime = new boolean[maxVal + 1];
-        Arrays.fill(isPrime, true);
-        if (maxVal >= 0) isPrime[0] = false;
-        if (maxVal >= 1) isPrime[1] = false;
-        for (int i = 2; i * i <= maxVal; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= maxVal; j += i) {
+    public void setPrime(boolean isPrime[],int n){
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=n;j+=i){
                     isPrime[j] = false;
                 }
             }
         }
+    }
+    public int  lowerBound(int arr[],int x){
+        int i = 0;
+        int j = arr.length-1;
+        int ind  = -1;
+        while(i<=j){
+            int m = i + (j-i)/2;
+            if(arr[m]>=x){
+                ind = m;
+                j= m-1;
+            }
+            else i = m+1;
+        }
+        return ind;
+    }
+    public int primeSubarray(int[] nums, int k) {
+        int maxi = Integer.MIN_VALUE;
+        int n= nums.length;
+        for(int x: nums){
+            maxi = Math.max(maxi,x);
+        }
+        boolean isPrime[] = new boolean[maxi+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0] =false;
+        isPrime[1] = false;
+        setPrime(isPrime,maxi);
 
-        int[] pref = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            pref[i + 1] = pref[i] + (isPrime[nums[i]] ? 1 : 0);
+        int pref[] = new int[n+1];
+        for(int i=0;i<n;i++){
+            pref[i+1] = pref[i] + (isPrime[nums[i]]? 1 : 0);
         }
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();
         int r = 0;
-        long ans = 0;
+        int count =0;
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int l=0;l<n;l++){
+            while(r<n){
+                if(isPrime[nums[r]]){
+                     map.put(nums[r],map.getOrDefault(nums[r],0)+1);
+                 }
 
-        for (int l = 0; l < n; l++) {
-            while (r < n) {
-                if (isPrime[nums[r]]) {
-                    map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
-                }
-
-                if (!map.isEmpty() && map.lastKey() - map.firstKey() > k) {
-                    if (isPrime[nums[r]]) {
-                        int c = map.get(nums[r]);
-                        if (c == 1) map.remove(nums[r]);
-                        else map.put(nums[r], c - 1);
-                    }
+                 if(!map.isEmpty() && map.lastKey()-map.firstKey()>k){
+                    if(isPrime[nums[r]]){
+                    int value = map.get(nums[r]);
+                    if(value == 1) map.remove(nums[r]);
+                    else map.put(nums[r],map.get(nums[r])-1);
                     break;
-                }
-                r++;
+                    }
+                 }
+                 r++;
             }
 
-            int need = pref[l] + 2;
-            int lo = 0, hi = pref.length;
-            while (lo < hi) {
-                int mid = (lo + hi) >>> 1;
-                if (pref[mid] >= need) hi = mid;
-                else lo = mid + 1;
-            }
-            if (lo < pref.length && lo <= r) {
-                ans += (r - lo + 1);
+            int need = pref[l]+2;
+            int idx = lowerBound(pref,need);
+            if(idx!=-1 && idx<=r ){
+                count+= (r-idx+1);
             }
 
-            if (isPrime[nums[l]]) {
-                int c = map.get(nums[l]);
-                if (c == 1) map.remove(nums[l]);
-                else map.put(nums[l], c - 1);
+            if(isPrime[nums[l]]){
+                int value = map.get(nums[l]);
+                if(value == 1) map.remove(nums[l]);
+                else map.put(nums[l],map.get(nums[l])-1);
             }
         }
+        return count;
 
-        return (int) ans;
     }
 }
