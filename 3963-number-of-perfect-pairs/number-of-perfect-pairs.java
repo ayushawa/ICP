f333e02054159ee367f570fd22c1883c79f6d8e7
ayushawasthi <devkali849@gmail.com>
2026-01-30 20:18:27 +0530

    Time: 49 ms (36.23%) | Memory: 88.8 MB (73.05%) - LeetSync

--- 3963-number-of-perfect-pairs/number-of-perfect-pairs.java
+++ 3963-number-of-perfect-pairs/number-of-perfect-pairs.java
@@ -6,11 +6,13 @@ class Solution {
             mod[i] = Math.abs((long)nums[i]);
         }
         Arrays.sort(mod);
-        int j=0;
+        int j =0;
         long count =0;
         for(int i=0;i<n;i++){
-            while(j<n && mod[j]<=2*mod[i]) j++;
-            count += j-i-1;
+            while(j<n && mod[j]<=2 * mod[i]){
+                j++;
+            }
+            count += (j-i-1);
         }
         return count;
     }
