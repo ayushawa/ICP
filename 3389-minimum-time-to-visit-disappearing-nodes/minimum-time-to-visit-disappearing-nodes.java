7fe163643d08bfbfc1a760130a043fe2395e6ec3
ayushawasthi <devkali849@gmail.com>
2026-02-04 19:53:31 +0530

    Time: 72 ms (81.45%) | Memory: 201.4 MB (77.69%) - LeetSync

--- 3389-minimum-time-to-visit-disappearing-nodes/minimum-time-to-visit-disappearing-nodes.java
+++ 3389-minimum-time-to-visit-disappearing-nodes/minimum-time-to-visit-disappearing-nodes.java
@@ -1,11 +1,3 @@
-class Touple{
-    int node,time,lost;
-    Touple(int node,int time,int lost){
-        this.node = node;
-        this.time = time;
-        this.lost = lost;
-    }
-}
 class Pair{
     int node;
     int t;
@@ -16,43 +8,37 @@ class Pair{
 }
 class Solution {
     public int[] minimumTime(int n, int[][] edges, int[] disappear) {
-        List<List<Touple>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++){
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         for(int i=0;i<edges.length;i++){
-            adj.get(edges[i][0]).add(new Touple(edges[i][1],edges[i][2],disappear[edges[i][1]]));
-            adj.get(edges[i][1]).add(new Touple(edges[i][0],edges[i][2],disappear[edges[i][0]]));
+            adj.get(edges[i][0]).add(new Pair(edges[i][1],edges[i][2]));
+            adj.get(edges[i][1]).add(new Pair(edges[i][0],edges[i][2]));
         }
-        int times[] = new int[n];
-        Arrays.fill(times,Integer.MAX_VALUE);
-        
-        PriorityQueue<Pair> q =new PriorityQueue<>((a,b)->a.t-b.t);
-        q.add(new Pair(0,0));
-        times[0] = 0;
-        while(!q.isEmpty()){
-            Pair  p = q.poll();
+        int dist[] = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[0] = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->a.t-b.t);
+        pq.add(new Pair(0,0));
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
             int node = p.node;
-            int t= p.t;
-            if (t > times[node]) continue;
-            for(Touple top: adj.get(node)){
-                int node2 = top.node;
-                int time = top.time;
-                int lostTime = top.lost;
+            int t = p.t;
+            if(dist[node]<t) continue;
 
-                int totaltime = time + t;
-                if(times[node2]>totaltime && totaltime<lostTime){
-                    times[node2] = totaltime;
-                    q.add(new Pair(node2,totaltime));
+            for(Pair p2 : adj.get(node)){
+                int neigh = p2.node;
+                int t2  = p2.t;
+                if(dist[neigh]>t+t2 && t+t2<disappear[neigh]){
+                    dist[neigh] = t+t2;
+                    pq.add(new Pair(neigh,t+t2));
                 }
             }
         }
-
-   
         for(int i=0;i<n;i++){
-            if(times[i] == Integer.MAX_VALUE) times[i] = -1;
-           
+            if(dist[i] == Integer.MAX_VALUE) dist[i] = -1;
         }
-        return times;
+        return dist;
     }
 }
