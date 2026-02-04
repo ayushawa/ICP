6cf6ef0d486817a5800a103395221131d6e19e4b
ayushawasthi <devkali849@gmail.com>
2026-02-04 18:01:19 +0530

    Time: 58 ms (33.90%) | Memory: 96.9 MB (66.10%) - LeetSync

--- 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
+++ 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
@@ -1,20 +1,20 @@
 class Solution {
     public int totalSteps(int[] nums) {
-        Stack<Integer> st =new Stack<>();
-        int dp[] = new int[nums.length];
-        int ans =0;
-        for(int i=0;i<nums.length;i++){
-            int maxcount = 0;
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int dp[] = new int[n];
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            int maxcount =0;
             while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
                 maxcount = Math.max(maxcount,dp[st.pop()]);
             }
             if(st.isEmpty()) dp[i] = 0;
-            else dp[i] = 1 + maxcount;
+            else dp[i] = maxcount + 1;
 
-            ans = Math.max(dp[i],ans);
+            ans = Math.max(ans,dp[i]);
             st.push(i);
         }
-
         return ans;
     }
 }
