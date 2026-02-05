6075d5027afe16736a7b733b69d870b3f4171354
ayushawasthi <devkali849@gmail.com>
2026-02-05 14:52:08 +0530

    Time: 1 ms (36.76%) | Memory: 43.2 MB (96.55%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node helper(Node head){
+        if(head == null) return head;
+        Node curr = head;
+        while(curr != null){
+            
+            if(curr.child != null){
+                Node next = curr.next;
+            
+                curr.next = helper(curr.child);
+                curr.next.prev = curr;
+                curr.child = null;
+
+                while(curr.next != null){
+                    curr = curr.next;
+                }
+                if(next != null){
+                    curr.next = next;
+                    next.prev = curr;
+                }
+
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+    public Node flatten(Node head) {
+        return helper(head);
+    }
+}
