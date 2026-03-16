07d37711c6827414bcf18b00352902ddc9d48319
ayushawasthi <devkali849@gmail.com>
2026-03-16 14:52:34 +0530

    Time: 12 ms (13.63%) | Memory: 71.2 MB (35.56%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,41 +1,27 @@
 class Solution {
-    public int helper(int arr[]){
-        int n  = arr.length;
-        int maxi = 0;
-        for(int i=0;i<n;i++){
-            int val =  arr[i];
-            int j = i-1;
-            int count= 1;
-            while(j>=0 && arr[j]>=val){
-                count++;
-                if(count>=val) break;
-                j--;
-            }
-
-            if(count<val){
-               j  = i+1;
-               while(j<n && arr[j]>=val){
-                    count++;
-                    if(count>=val) break;
-                      j++;
-                 }
-            }
-            if(count>=val) maxi = Math.max(maxi,val*val);
+    int ans = 0;
+    public int helper(char matrix[][],int i,int j,int dp[][]){
+        if(i == matrix.length || j== matrix[0].length || matrix[i][j]=='0') {
+            return 0;
         }
-        return maxi ;
+        if(dp[i][j] != -1) return dp[i][j];
+        int square =  Math.min(helper(matrix,i+1,j+1,dp),Math.min(helper(matrix,i+1,j,dp),helper(matrix,i,j+1,dp))) + 1;
+        ans = Math.max(ans,square);
+        return dp[i][j] = square;
     }
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int arr[] = new int[m];
-        int maxi = 0;
+        int dp[][] = new int[n][m];  
         for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        } 
+        for(int i =0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j] == '0') arr[j] = 0;
-                else arr[j] += 1;
+               if(matrix[i][j] == '1') ans = Math.max(ans,helper(matrix,i,j,dp));
             }
-            maxi = Math.max(maxi,helper(arr));
         }
-        return maxi;
+        return ans * ans;
+        
     }
 }
