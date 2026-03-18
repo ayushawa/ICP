a0f59687b84401d2518733f33b748af91527af1f
ayushawasthi <devkali849@gmail.com>
2026-03-18 08:33:20 +0530

    Time: 3 ms (14.10%) | Memory: 47.2 MB (6.43%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -14,24 +14,23 @@
  * }
  */
 class Solution {
-    public int helper(TreeNode root,int res[],HashMap<TreeNode,Integer> map){
-      if(root == null) return 0;
-      if(map.containsKey(root)) return map.get(root);
-      int leftNotTake = helper(root.left,res,map);
-      int rightNotTake = helper(root.right,res,map);
-      int leftMax =0;
-      if(root.left != null) leftMax+=  helper(root.left.left,res,map)+helper(root.left.right,res,map);
-      int rightMax = 0;
-      if(root.right != null) rightMax = helper(root.right.left,res,map)+helper(root.right.right,res,map);
-      res[0]  = Math.max(res[0],Math.max(leftNotTake+rightNotTake,root.val + leftMax + rightMax));
-      map.put(root,Math.max(leftNotTake+rightNotTake,root.val + leftMax+rightMax));
-      return Math.max(leftNotTake+rightNotTake,root.val + leftMax+rightMax) ;
+    public int helper(TreeNode root,HashMap<TreeNode,Integer> map){
+         if(root == null) return 0;
+         if(map.containsKey(root)) return map.get(root);
+
+         int takeLeft = 0;
+         if(root.left != null) takeLeft = helper(root.left.left,map)+helper(root.left.right,map);
+         int takeright = 0;
+         if(root.right !=null) takeright = helper(root.right.left,map) + helper(root.right.right,map);
+         map.put(root,Math.max(root.val+ takeLeft+takeright, helper(root.left,map)+helper(root.right,map)));
+         return Math.max(root.val  + takeLeft+takeright, helper(root.left,map)+helper(root.right,map));
+
     }
     public int rob(TreeNode root) {
-        int res[] = {root.val};
-         HashMap<TreeNode,Integer> map = new HashMap<>();
-        helper(root,res,map);
        
-        return res[0];
+         HashMap<TreeNode,Integer> map = new HashMap<>();
+       return  helper(root,map);
+       
+        
     }
 }
