e46ef72ffee0c066a0969bd699a1706f588fe652
ayushawasthi <devkali849@gmail.com>
2026-04-12 16:56:10 +0530

    Time: 16 ms (48.46%) | Memory: 43.5 MB (81.54%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.java
@@ -1,33 +1,26 @@
-
 class Solution {
-
-    int dist(int a, int b) {
-        int x1 = a / 6, y1 = a % 6;
-        int x2 = b / 6, y2 = b % 6;
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    public int dis(int a,int b){
+        int x1 = a/6, y1=a%6;
+        int x2 = b/6 ,y2 = b%6;
+        return Math.abs(x2-x1) + Math.abs(y1-y2);
     }
-
     public int minimumDistance(String word) {
-        int[] dp = new int[26];
+        int dp[] = new int[26];
+        for(int i= 1;i<word.length();i++){
+            int curr = word.charAt(i)-'A';
+            int prev = word.charAt(i-1)-'A';
 
-        for (int i = 1; i < word.length(); i++) {
-            int curr = word.charAt(i) - 'A';
-            int prev = word.charAt(i - 1) - 'A';
-
-            int[] newDp = new int[26];
-            Arrays.fill(newDp, Integer.MAX_VALUE);
-
-            for (int j = 0; j < 26; j++) {
-                newDp[j] = Math.min(newDp[j], dp[j] + dist(prev, curr));
-                newDp[prev] = Math.min(newDp[prev], dp[j] + dist(j, curr));
+            int newdp[] = new int[26];
+            Arrays.fill(newdp,Integer.MAX_VALUE);
+            for(int j=0;j<26;j++){
+                newdp[j] = Math.min(newdp[j],dp[j]+dis(prev,curr));
+                newdp[prev] = Math.min(newdp[prev],dp[j]+dis(j,curr));
             }
-
-            dp = newDp;
+            dp = newdp;
         }
 
-        int ans = Integer.MAX_VALUE;
-        for (int x : dp) ans = Math.min(ans, x);
-
-        return ans;
+        int mini = Integer.MAX_VALUE;
+        for(int x: dp) mini = Math.min(mini,x);
+        return mini;
     }
 }
